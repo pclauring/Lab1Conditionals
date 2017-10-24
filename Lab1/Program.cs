931fe7cabb8becf66bfecb2c6330e1f812934128
Pierce Lauring <pclauring@gmail.com>
2017-10-24 18:58:14 -0400

    Final Edits on master for lab 1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab1
 {
@@ -10,6 +6,75 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            double lengthClass, heightClass, widthClass;
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.Write("Enter the Length of the Classroom: ");
+
+                lengthClass = ValidationOfDec();
+
+                Console.Write("Enter the Height of the Classroom: ");
+
+                heightClass = ValidationOfDec();
+
+                Console.Write("Enter the Width of the Classroom: ");
+
+                widthClass = ValidationOfDec();
+
+                double areaOfClassroom = widthClass * lengthClass;
+                double perimeterOfClassroom = 2 * widthClass + 2 * lengthClass;
+                double volumeOfClassroom = heightClass * widthClass * lengthClass;
+
+                Console.WriteLine("The area of the room is: " + areaOfClassroom);
+                Console.WriteLine("The perimeter of the room is: " + perimeterOfClassroom);
+                Console.WriteLine("The volume of the room is: " + volumeOfClassroom);
+
+                bool validAnswer = true;
+
+
+                Console.Write("Would the user like to find the measurements of another room (y/n)?: ");
+
+                while (validAnswer)
+                {
+                    string repeatPrompt = Console.ReadLine();
+                    if (repeatPrompt == "y" || repeatPrompt == "Y")
+                    {
+                        repeat = true;
+                        validAnswer = false;
+                    }
+                    else if (repeatPrompt == "n" || repeatPrompt == "N")
+                    {
+                        Console.WriteLine("Okay exiting the program now.");
+                        repeat = false;
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Didn't enter a valid answer. Do you want to find the measurements of another room (y/n)?: ");
+                        validAnswer = true;
+                    }
+                }
+            }
+        }
+        private static double ValidationOfDec()
+        {
+            while (true)
+            {
+                string dimension = Console.ReadLine();
+                bool success = double.TryParse(dimension, out double dimensionDec);
+
+                if (success)
+                {
+                    return dimensionDec;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid measurement");
+                }
+
+            }
         }
     }
 }
